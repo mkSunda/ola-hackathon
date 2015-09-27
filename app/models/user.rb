ae61928053f7fdc10162a918808bcbe30907960c
Mukesh <mksunda01@gmail.com>
2015-09-27 11:32:49 +0530

    bug fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,14 +12,14 @@ class User < ActiveRecord::Base
 			 :driver_name => ride.driver_name, :driver_number => ride.driver_number, :cab_number => ride.cab_number,
 			 :car_model => ride.car_model, :confirmed => true}
 			else
-				output = {:category => "Sedan", :pickup_time => ride.pickup_time.strftime("%I:%M %p"), :confirmed => false}
+				output = {:category => "Sedan", :pickup_time => scheduled_booking.pickup_time.strftime("%I:%M %p"), :confirmed => false}
 			end
 		elsif not ride.nil?
 			output = {:category => "Sedan", :pickup_time => ride.arrival_time.strftime("%I:%M %p"),
 			 :driver_name => ride.driver_name, :driver_number => ride.driver_number, :cab_number => ride.cab_number,
 			 :car_model => ride.car_model, :confirmed => true}
 		elsif not scheduled_booking.nil?
-			output = {:category => "Sedan", :pickup_time => ride.pickup_time.strftime("%I:%M %p"), :confirmed => false}
+			output = {:category => "Sedan", :pickup_time => scheduled_booking.pickup_time.strftime("%I:%M %p"), :confirmed => false}
 		end
 		output
   end
